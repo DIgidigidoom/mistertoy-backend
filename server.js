efb5246ab942a6212fee866b3ae4ef39c185eeca
Digidigidoom <tomshahar91@gmail.com>
2025-05-05 13:58:52 +0300

    Add fix to backend

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express()
 
 const corsOptions = {
     origin: [
-        'http://localhost:3000',
+        'http://localhost:3030',
         'http://localhost:5173',
-        'http://127.0.0.1:3000',
+        'http://127.0.0.1:3030',
         'http://127.0.0.1:5173'
     ],
     credentials: true
